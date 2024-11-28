56fa08aa9994683400ce5d172de1070a7f484b5b
Clothilde Brooker <clothilde.brkr@gmail.com>
2024-11-28 15:00:26 +0000

    generation des models

---
+++ models/staging/bdd_projet/stg_bdd_projet__pop_france_2023.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ source('bdd_projet', 'pop_france_2023') }}
+
+),
+
+renamed as (
+
+    select
+        string_field_0 AS num_departement,
+        string_field_1 AS intitule_departement,
+        string_field_2 AS pop_totale
+
+    from source
+
+)
+
+select * from renamed
