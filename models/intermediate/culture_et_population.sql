54de586897c4bfd342973025fff04510fe1ed472
Clothilde Brooker <clothilde.brkr@gmail.com>
2024-11-29 11:50:21 +0000

    j'ai fait le ratio, des équipements par population version finale

--- models/intermediate/culture_et_population.sql
+++ models/intermediate/culture_et_population.sql
@@ -26,15 +26,38 @@ GROUP BY intitule_departement
 
 ,join_sport AS (
 SELECT *
-FROM aggrega
-INNER JOIN {{ ref('stg_bdd_projet__etablissements_sportifs') }}
+FROM {{ ref('clean_sport') }}
+INNER JOIN aggrega
 ON intitule_departement = nom_departement
 )
+
+, count_installations AS (
+SELECT 
+intitule_departement,
+total_popu,
+count_equipement,
+COUNT(nom_equipement) AS count_instal_sportive
+FROM join_sport
+GROUP BY intitule_departement, total_popu, count_equipement
+) 
+
+, sum_equipements AS (
+SELECT 
+intitule_departement,
+total_popu,
+count_equipement,
+count_instal_sportive,
+SUM(count_instal_sportive + count_equipement) AS total_equipements
+FROM count_installations
+GROUP BY intitule_departement, total_popu, count_equipement, count_instal_sportive
+) 
 
 SELECT 
 intitule_departement,
 total_popu,
 count_equipement,
-(count_equipement/total_popu) AS ratio_equipement 
-FROM join_sport
-GROUP BY intitule_departement, total_popu, count_equipement
+count_instal_sportive,
+total_equipements, 
+(total_equipements/total_popu) AS ratio_equipement_popu
+FROM sum_equipements
+
