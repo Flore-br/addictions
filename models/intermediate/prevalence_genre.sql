28f36baa208905bb20a79ab5f4f8a73465656124
Lyes Bouabdallah <lyes.bouabdallah@gmail.com>
2024-11-29 14:56:51 +0000

    prévalence par sexe

--- models/intermediate/prevalence_genre.sql
+++ models/intermediate/prevalence_genre.sql
@@ -1,4 +1,5 @@
-select 
-(sum(ntop)/sum(npop)) 
-from {{ ref('For_mapping') }}
-WHERE libelle_sexe = "hommes"
+SELECT 
+libelle_sexe,
+(sum(ntop)/sum(npop)) AS prevalence_sexe
+FROM {{ ref('For_mapping') }}
+GROUP BY libelle_sexe
