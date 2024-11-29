59b79f255e2272f54ffd73bbeea8c950d9326b51
Flore Berqué <flore_berque@yahoo.com>
2024-11-29 11:05:50 +0000

    modifications nbre psychiatres

--- models/intermediate/nbre_psychiatres_departements_2023.sql
+++ models/intermediate/nbre_psychiatres_departements_2023.sql
@@ -1,14 +1,13 @@
-SELECT DISTINCT
-LOWER (
-    TRANSLATE(
-    libelle_departement,
-    'àèéêîòôùû',
-    'aeeeioouu'
-  ))AS libelle_departement,
-secteur_conventionnel,
-SUM (effectif) as effectif_psychiatres
-FROM {{ ref('stg_bdd_projet__repartition_psychiatre') }}
-WHERE libelle_departement NOT IN ("Tout département")
-AND date_date = '2023-01-01'
-GROUP BY libelle_departement, secteur_conventionnel
-ORDER BY libelle_departement, secteur_conventionnel ASC
+select distinct
+    departement as n_departement,
+    lower(
+        translate(libelle_departement, 'àèéêîòôùû', 'aeeeioouu')
+    ) as libelle_departement,
+    secteur_conventionnel,
+    sum(effectif) as effectif_psychiatres
+from {{ ref("stg_bdd_projet__repartition_psychiatre") }}
+where
+    libelle_departement not in ("Tout département")
+    and date_date = '2023-01-01'
+group by libelle_departement, departement, secteur_conventionnel
+order by libelle_departement, secteur_conventionnel asc
