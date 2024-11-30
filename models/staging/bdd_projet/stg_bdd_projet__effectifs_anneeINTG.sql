603e5ca12f804037628e2b6a2b5faed0342cef32
Lyes Bouabdallah <lyes.bouabdallah@gmail.com>
2024-11-30 11:49:23 +0000

    schema.yml

---
+++ models/staging/bdd_projet/stg_bdd_projet__effectifs_anneeINTG.sql
@@ -0,0 +1,33 @@
+with 
+
+source as (
+
+    select * from {{ source('bdd_projet', 'effectifs') }}
+
+),
+
+renamed as (
+
+    select
+        CAST(CAST(annee AS INT) AS STRING) AS annee_corrigee
+        patho_niv1,
+        patho_niv2,
+        patho_niv3,
+        cla_age_5,
+        sexe,
+        region,
+        dept,
+        ntop,
+        npop,
+        prev,
+        `niveau prioritaire` AS niveau_prio,
+        libelle_classe_age,
+        libelle_sexe,
+        tri
+
+    from source
+
+)
+
+select * from renamed
+WHERE cla_age_5 NOT IN ('00-04','05-sept.', 'oct.-14','5-sept','95et+')
