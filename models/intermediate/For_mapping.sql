080d719385098ac13f4b62167dde40dd2ca4ef5c
Lyes Bouabdallah <lyes.bouabdallah@gmail.com>
2024-11-30 14:58:41 +0000

    schema.yml

--- models/intermediate/For_mapping.sql
+++ models/intermediate/For_mapping.sql
@@ -1,5 +1,10 @@
 SELECT 
-    DATE(annee, 1, 1) AS annee_date,
+    CASE 
+        WHEN SAFE_CAST(CAST(annee AS STRING) || '-01-01' AS DATE) IS NOT NULL THEN 
+            DATE(CAST(annee AS STRING) || '-01-01')
+        ELSE 
+            NULL
+    END AS annee_date,
     patho_niv1,
     cla_age_5 AS age,
     region_name AS region,
@@ -7,11 +12,16 @@ SELECT
     ecv.dept,
     Ntop,
     Npop,
-    prev,	
+    prev,    
     libelle_sexe,
-    tp.taux_pauvrete,
-FROM {{ ref('stg_bdd_projet__effectifs') }} AS ecv
-LEFT JOIN {{ ref('stg_bdd_projet__dept_region_name') }} AS drn
+    tp.taux_pauvrete
+FROM `projet-final-le-wagon-442809`.`dbt_lbouabdallah`.`stg_bdd_projet__effectifs` AS ecv
+LEFT JOIN `projet-final-le-wagon-442809`.`dbt_lbouabdallah`.`stg_bdd_projet__dept_region_name` AS drn
     ON ecv.dept = drn.dept1
-LEFT JOIN {{ ref('stg_bdd_projet__taux_pauvreté') }} AS tp
-    ON ecv.dept = tp.code_departement 
+LEFT JOIN `projet-final-le-wagon-442809`.`dbt_lbouabdallah`.`stg_bdd_projet__taux_pauvreté` AS tp
+    ON ecv.dept = tp.code_departement;
+
+
+
+
+
