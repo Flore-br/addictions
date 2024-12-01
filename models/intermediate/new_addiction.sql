e1a35e96f9c3fd5d12ca8bbc027ac6eba5398e12
Clothilde Brooker <clothilde.brkr@gmail.com>
2024-12-01 20:37:33 +0000

    ajout comorbidité

--- models/intermediate/new_addiction.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-*,
-DATE(CONCAT(annee, '-01-01')) AS date_date
-FROM {{ ref('stg_bdd_projet__Commorbidites') }}
-WHERE patho_niv1 = "Maladies psychiatriques" AND patho_niv2 = "Troubles addictifs" AND patho_niv3 = "Troubles addictifs"
