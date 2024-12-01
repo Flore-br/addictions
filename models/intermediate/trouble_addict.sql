091dfc1372e43fcb115fe6cfb7163e6d5e710dd2
Clothilde Brooker <clothilde.brkr@gmail.com>
2024-12-01 19:43:02 +0000

    change date fromat

--- models/intermediate/trouble_addict.sql
+++ models/intermediate/trouble_addict.sql
@@ -1,4 +1,14 @@
 SELECT 
-*
+annee,
+-- SAFE_CAST(annee AS date),
+patho_niv3,
+comorbidite,
+libelle_comorbidite,
+region,
+dept,
+ncomorb,
+ntop,
+proportion_comorb,
+patho_niv2_comorb,
 FROM {{ ref('stg_bdd_projet__Commorbidites') }}
 WHERE patho_niv1 = "Maladies psychiatriques" AND patho_niv2 = "Troubles addictifs" AND patho_niv3 = "Troubles addictifs" AND annee > 2019
