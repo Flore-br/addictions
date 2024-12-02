75bddf41b993a99d9b2f5a1174d3a3f3ad6ff664
Constantin Beraud <constantinberaud7@gmail.com>
2024-12-02 10:48:41 +0000

    commit

---
+++ models/intermediate/For_mapping.sql
@@ -0,0 +1,17 @@
+SELECT 
+annee,
+    patho_niv1,
+    cla_age_5 AS age,
+    region_name AS region,
+    dept_name AS departement,
+    ecv.dept,
+    Ntop,
+    Npop,
+    IFNULL (prev, 0) AS prev,
+    libelle_sexe,
+    tp.taux_pauvrete
+FROM {{ ref('bdd_projet_effectifs_V3') }} AS ecv
+LEFT JOIN `projet-final-le-wagon-442809`.`dbt_lbouabdallah`.`stg_bdd_projet__dept_region_name` AS drn
+    ON ecv.dept = drn.dept1
+LEFT JOIN `projet-final-le-wagon-442809`.`dbt_lbouabdallah`.`stg_bdd_projet__taux_pauvreté` AS tp
+    ON ecv.dept = tp.code_departement
