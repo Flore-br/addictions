e311688d76a43a717ce78a8f2c4764fb8af4869b
Clothilde Brooker <clothilde.brkr@gmail.com>
2024-12-02 13:50:15 +0000

    cleaning des départements pour effectif V3

--- models/intermediate/effectif_with_dept_name.sql
+++ models/intermediate/effectif_with_dept_name.sql
@@ -1,5 +1,23 @@
+WITH clean_dept AS (
 SELECT
-*
+*,
+CASE
+    WHEN dept= '1' THEN '01'
+    WHEN dept= '2' THEN '02'
+    WHEN dept= '3' THEN '03'
+    WHEN dept= '4' THEN '04'
+    WHEN dept= '5' THEN '05'
+    WHEN dept= '6' THEN '06'
+    WHEN dept= '7' THEN '07'
+    WHEN dept= '8' THEN '08'
+    WHEN dept= '9' THEN '09'
+    ELSE dept
+END AS n_departement,
 FROM {{ ref('bdd_projet_effectifs_V3') }}
+)
+
+SELECT 
+*,
+FROM clean_dept
 JOIN {{ ref('departements_clean') }} 
 ON dept = dept1
