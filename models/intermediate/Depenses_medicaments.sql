f79c8c1407650d38d96842385d46057c704e953b
Lyes Bouabdallah <lyes.bouabdallah@gmail.com>
2024-12-03 09:28:05 +0000

    Medicaments

--- models/intermediate/Depenses_medicaments.sql
+++ models/intermediate/Depenses_medicaments.sql
@@ -1,11 +1,21 @@
-SELECT
-*
+WITH fusion as (SELECT
+'2020' as annee,
+classe_ephmra,
+montant_rembours____2020 as montant
 FROM {{ ref('stg_bdd_projet__Depenses_medicaments_2020') }}
 UNION ALL
 SELECT
-*
+'2021' as annee,
+classe_ephmra,
+montant_rembours____2021 as montant
 FROM {{ ref('stg_bdd_projet__Depenses_medicaments_2021') }}
 UNION ALL
 SELECT
-*
-FROM {{ ref('stg_bdd_projet__Depenses medicaments_2022') }}
+'2022' as annee,
+classe_ephmra,
+montant_rembours____2022 as montant
+FROM {{ ref('stg_bdd_projet__Depenses medicaments_2022') }} ) 
+
+SELECT *
+FROM fusion
+
